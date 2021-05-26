e28f1931d780d81bebbc99590af3328aaf316304
abbuyanaa <abbuyanaa@gmail.com>
2021-05-26 21:42:12 +0800

    Modal dotor OrderSummary componentiig oruulsan

--- src/components/BuildControls/index.js
+++ src/components/BuildControls/index.js
@@ -3,25 +3,19 @@ import BuildControl from "../BuildControl";
 import styles from "./style.module.css";
 
 const BuildControls = (props) => {
-  const controls = {
-    bacon: "Гахайн мах",
-    cheese: "Бяслаг",
-    meat: "Үхрийн мах",
-    salad: "Салад",
-  };
   return (
     <div className={styles.BuildControls}>
       <p>
         Бургерийн үнэ: <strong>{props.price}</strong>
       </p>
-      {Object.keys(controls).map((el) => (
+      {Object.keys(props.ingredientsNames).map((el) => (
         <BuildControl
           key={el}
           ortsNemeh={props.ortsNemeh}
           ortsHasah={props.ortsHasah}
           disabled={props.disabledIngredients}
           type={el}
-          orts={controls[el]}
+          orts={props.ingredientsNames[el]}
         />
       ))}
       <button disabled={props.disabled} className={styles.OrderButton}>
--- src/pages/BurgerPage/index.js
+++ src/pages/BurgerPage/index.js
@@ -2,8 +2,15 @@ import React, { Component } from "react";
 import Burger from "../../components/Burger";
 import BuildControls from "../../components/BuildControls";
 import Modal from "../../components/General/Modal";
+import OrderSummary from "../../components/OrderSummary";
 
 const INGREDIENT_PRICES = { salad: 150, cheese: 250, bacon: 800, meat: 1500 };
+const INGREDIENT_NAMES = {
+  bacon: "Гахайн мах",
+  cheese: "Бяслаг",
+  meat: "Үхрийн мах",
+  salad: "Салад",
+};
 
 class BurgerPage extends Component {
   state = {
@@ -55,11 +62,14 @@ class BurgerPage extends Component {
     return (
       <div>
         <Modal>
-          <h1>Та итгэлтэй байна уу...</h1>
-          <p>Захиалгын дэлгэрэнгүй</p>
+          <OrderSummary
+            ingredientsNames={INGREDIENT_NAMES}
+            ingredients={this.state.ingredients}
+          />
         </Modal>
         <Burger orts={this.state.ingredients} />
         <BuildControls
+          ingredientsNames={INGREDIENT_NAMES}
           disabled={!this.state.purchasing}
           price={this.state.totalPrice}
           disabledIngredients={disabledIngredients}
